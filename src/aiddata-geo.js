6c7941c1f46d672f0e27109ee5616afef00fd5bc
Maddie Nicolas <mcpnicolas@gmail.com>
2019-04-11 00:18:18 -0400

    Implemented Vis1ChartConfig for SVG and scales and bars for drawing Vis1Chart

--- src/aiddata-geo.js
+++ src/aiddata-geo.js
@@ -35,13 +35,90 @@ function groupByCountry(data) {
 	},{})
 
 	result = Object.keys(result).map(key => result[key])
+	result.sort((a, b) => {
+		return d3.descending(a.AmountDonated-a.AmountReceived, b.AmountDonated-b.AmountReceived)
+		})
   return result
 }
 
+function getVis1ChartConfig() {
+	let width = 700;
+	let height = 500;
+	let margin = {
+		top: 30,
+		bottom: 10,
+		left: 100,
+		right: 10
+	}
+	let bodyHeight = height - margin.top - margin.bottom
+	let bodyWidth = width - margin.left - margin.right
+
+	let container = d3.select("#Vis1Chart")
+	
+	container
+		.attr("width", width)
+		.attr("height", height)
+
+	return { width, height, margin, bodyHeight, bodyWidth, container }
+}
+
+function getVis1ChartScales(countries, config) {
+	let { bodyWidth, bodyHeight } = config;
+	let maxAmount = Math.max(d3.max(countries, c => c.AmountDonated),d3.max(countries, c => c.AmountReceived))
+	
+	let yScale = d3.scaleBand()
+		.domain(countries.map(c => c.Country))
+		.range([0,bodyHeight])
+		.padding(0.2)
+
+	let xScale = d3.scaleLinear()
+		.domain([0, maxAmount])
+		.range([0, bodyWidth/2])
+
+	return { xScale, yScale }
+}
+
+function drawBarsVis1Chart(countries, scales, config) {
+	let { margin, container, bodyWidth } = config
+	let { xScale, yScale } = scales
+
+	let body = container.append("g")
+	.style("transform", `translate(${margin.left}px,${margin.top}px)`)
+
+	let bars = body.selectAll(".bar").data(countries)
+
+	bars.enter().append("rect")
+		.attr("y", (d) => yScale(d.Country))
+		.attr("x", (bodyWidth/2)+margin.right)
+		.attr("height", yScale.bandwidth())
+		.attr("width", (d) => xScale(d.AmountReceived))
+		.attr("fill", "#1F4F50")
+
+	bars.enter().append("rect")
+		.attr("y", (d) => yScale(d.Country))
+		.attr("x", bodyWidth/2)
+		.attr("height", yScale.bandwidth())
+		.attr("width", (d) => xScale(d.AmountDonated))
+		.attr("fill", "#C96F69")
+		.attr("transform", `translate(${bodyWidth}, 0) scale(-1, 1)`)
+}
+
+function drawAxesVis1Chart(airlines, scales, config) {
+
+}
+
+function drawVis1Chart(countries) {
+	let config = getVis1ChartConfig()
+	let scales = getVis1ChartScales(countries, config)
+	drawBarsVis1Chart(countries, scales, config)
+	drawAxesVis1Chart(countries, scales, config)
+}
+
 function showData() {
-	let aiddata = store.aiddata;
+	let aiddata = store.aiddata
 	let countries = groupByCountry(aiddata)
 	console.log(countries)
+	drawVis1Chart(countries)
 }
 
 loadData().then(showData);
